82ab99a568d245d88752871514f48bd5a7e892b8
Isaac Smith <smithi@mail.gvsu.edu>
2015-09-27 19:57:16 -0400

    Update GameStart.java

--- GameStart.java
+++ GameStart.java
@@ -1,32 +1,37 @@
 package com.example.isaacsmith.tba;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Timer;
+
 public class GameStart extends AppCompatActivity {
 
     private int score;
+    private CountDownTimer countDownTimer;
+    private boolean timerHasStarted = false;
+    public TextView start;
+    private final long startTime = 30 * 1000;
+    private final long interval = 1 * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_start);
         Button clickitbtn = (Button) findViewById(R.id.ClickIt);
+        final MyCountDownTimer countDownTimer = new MyCountDownTimer(startTime, interval);
+        start = (TextView) this.findViewById(R.id.timeleft);
+        start.setText(start.getText() + String.valueOf(startTime / 1000));
 
-        clickitbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                score = score + 1;
-                TextView scoredisp = (TextView) findViewById(R.id.addDispScore);
-                scoredisp.setText("Score: " + score);
-            }
-        });
+
     }
 
     @Override
@@ -49,5 +54,41 @@ public class GameStart extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class MyCountDownTimer extends CountDownTimer {
+        public  MyCountDownTimer(long startTime, long interval){
+            super(startTime, interval);
+            start();
+            timerHasStarted = true;
+            Button clickitbtn = (Button) findViewById(R.id.ClickIt);
+            clickitbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    boolean checks = getTimerStart();
+                    if(checks = true) {
+                        score = score + 1;
+                        TextView scoredisp = (TextView) findViewById(R.id.addDispScore);
+                        scoredisp.setText("Score: " + score);
+                    }else{
+                        score = score;
+                    }
+                }
+            });
+        }
+
+        public boolean getTimerStart(){
+            return timerHasStarted;
+        }
+        @Override
+        public void onFinish(){
+            start.setText("Time's up!");
+            timerHasStarted = false;
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished){
+            start.setText("" + millisUntilFinished / 1000);
+        }
     }
 }
